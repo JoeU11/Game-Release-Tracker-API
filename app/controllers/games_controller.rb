1c8a8fb465ee9dfac02c7da43554b55ab9339bef
joe <joe.ulica@gmail.com>
2023-01-18 15:05:57 -0500

    create REST routes for games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,35 @@
 class GamesController < ApplicationController
   def index
-    render Game.all.as_json
+    render json: Game.all.as_json
+  end
+
+  def show
+    render json: Game.find(params[:id]).as_json
+  end
+
+  def create
+    game = Game.new(name: params[:name], description: params[:description], anticipation: params[:anticipation])
+    if params[:year]
+      game.release_date = DateTime.new(params[:year], params[:month], params[:day])
+    end
+    game.save
+    render json: game.as_json
+  end
+
+  def update
+    game = Game.find(params[:id])
+    game.name = params[:name] || game.name
+    game.description = params[:description] || game.description
+    if params[:year]
+      game.release_date = DateTime.new(params[:year], params[:month], params[:day]) || game.release_date
+    end
+    game.anticipation = params[:anticipation] || game.anticipation
+    game.save
+    render json: game.as_json
+  end
+
+  def destroy
+    Game.find(params[:id]).destroy
+    render json: {message: "Game has been removed"}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get "games" => "games#index"
   get "games/:id" => "games#show"
   post "games" => "games#create"
-  patch "games/:id" => "games#patch"
+  patch "games/:id" => "games#update"
   delete "games/:id" => "games#destroy"
   
 end
